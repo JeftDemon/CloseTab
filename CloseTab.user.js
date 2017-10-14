c32227c69a9af45a1d501530b0abfd12a8ed684b
JeftDemon <32802064+JeftDemon@users.noreply.github.com>
2017-10-14 16:27:19 -0300

    Create CloseTab.user.js

---
+++ CloseTab.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         CloseTab
+// @namespace    https://github.com/Mokiller/CloseTab
+// @version      1.3
+// @description  Close tabs when the barload is complete.
+// @author       Mokiller
+// @match        http://bitcofarm.com/modules/*
+// @match        http://bitcomine.net/modules/*
+// @match        http://ethero.net/modules/*
+// @updateURL    https://github.com/Mokiller/CloseTab/raw/master/CloseTab.meta.js
+// @downloadURL  https://github.com/Mokiller/CloseTab/raw/master/CloseTab.user.js
+// @supportURL   https://github.com/Mokiller/CloseTab/issues
+// @connect      *
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+(function()
+	{
+		'use strict';
+
+		window.focus();
+		setInterval(function()
+			{
+				var comp = $("#desc").contents().first().text();
+				if(comp == "Completed!" || comp == "Completed")
+					{
+						window.close();
+					}
+			},3000);
+	})();
