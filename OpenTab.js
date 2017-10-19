ef644de821ea61a8cbca36f515c165389097a044
JeftDemon <32802064+JeftDemon@users.noreply.github.com>
2017-10-19 17:29:56 -0200

    Create OpenTab.js

---
+++ OpenTab.js
@@ -0,0 +1,39 @@
+(function()
+    {
+        'use strict';
+        var myLinks = [];
+        $("#right a").each(function()
+            {
+                if($(this).find(".disabled_pbx").length === 0)
+                    {
+                        myLinks.push(this);
+                    }
+            });
+        var totalLinks = myLinks.length;
+        setInterval(function()
+            {
+                if(myLinks.length > 0)
+                    {
+                        var rank = totalLinks - myLinks.length;
+                        var url = $(myLinks.pop()).attr("href");
+                        console.log(rank + "de" + totalLinks + " :: clicakado em " + url);
+                        var myWindowADS;
+                        /*if (!myWindowADS)
+                            {
+                                myWindowADS = window.open(url);
+                            }
+                        else
+                            {
+                                if (myWindowADS.closed)
+                                    {
+                                        myWindowADS.close();
+                                    }
+                            }*/
+                    }
+                else
+                    {
+                        console.log("finalizado!!");
+                    }
+ 
+            },42000);
+    })();
